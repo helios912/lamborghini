fe0db0f74ace0a094e41a8432f5f6e9e569918d4
Mukola2 <janaon333@gmail.com>
2024-02-19 20:47:11 +0200

    adaptacia

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
+const menuBTN = document.querySelector(".menu-btn");
+const menuMobile = document.querySelector(".menu-mobile");
+
+menuBTN.addEventListener("click", () => {
+  menuMobile.classList.toggle("menu--open");
+});
+
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
-
   loop: true,
-  slidesPerView: 2,
-  spaseBetween: 20,
 
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-right",
     prevEl: ".swiper-button-left",
   },
-
+  breackpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
   // And if we need scrollbar
 });
